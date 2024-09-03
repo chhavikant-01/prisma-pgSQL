966b3f639c2a9576c8f5dd829435004fef86386c
Chhavikant Mahobia <ckcodes907@gmail.com>
2024-09-03 08:04:46 +0530

    data now reflects on db

--- script.ts
+++ script.ts
@@ -2,8 +2,18 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+    await prisma.user.deleteMany({})
+    const user = await prisma.user.create({
+        data: {
+            name: 'Alice',
+            email: 'alice@gmail.com',
+            age: 32,
+        }, include: {
+            userPreferences: true
+        }
+    })
 
-    await prisma.user.deleteMany()
+    console.log('Created user:', user)
 
 }
 
